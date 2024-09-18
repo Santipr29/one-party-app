10fb23746e4211720d854afaf47ce720ec03c031
Santiago <santipr2004@gmail.com>
2024-09-18 12:39:37 -0500

    Feat: correction logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Body from './components/Body';
 function App() {
   return (
     <div className="App">
-      <img src={logo}/>
+      <img src={logo} className='logo'/>
       <Body/>
     </div>
   );
